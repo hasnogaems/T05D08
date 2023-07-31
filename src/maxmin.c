f288002ddb3ff6338b7af4b1eb55c0246c18f2b3
Medusa Galadriel <medusaga@ox-a5.msk.21-school.ru>
2023-07-31 19:38:53 +0300

    m1

--- src/maxmin.c
+++ src/maxmin.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void maxmin(int *prob1, int *prob2, int *prob3, int *max, int *min);
+void maxmin(int prob1, int prob2, int prob3, int *max, int *min);
 
 /* Find a max & min probabilities */
 int main() {
@@ -12,7 +12,7 @@ int main() {
 
     int max, min;
 
-    maxmin(&x, &y, &z, &max, &min);
+    maxmin(x, y, z, &max, &min);
 
     printf("%d %d", max, min);
 
@@ -21,12 +21,12 @@ int main() {
 
 /* This function should be kept !!! (Your AI) */
 /* But errors & bugs should be fixed         */
-void maxmin(int *prob1, int *prob2, int *prob3, int *max, int *min) {
-    *max = *min = *prob1;
+void maxmin(int prob1, int prob2, int prob3, int *max, int *min) {
+    *max = *min = prob1;
 
-    if (*prob2 > *max) *max = *prob2;
-    if (*prob2 < *min) *min = *prob2;
+    if (prob2 > *max) *max = prob2;
+    if (prob2 < *min) *min = prob2;
 
-    if (*prob3 > *max) *max = *prob3;
-    if (*prob3 < *min) *min = *prob3;
+    if (prob3 > *max) *max = prob3;
+    if (prob3 < *min) *min = prob3;
 }
