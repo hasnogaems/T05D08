6dfee2723680fe7b52cf98b9c3cc7ae3d5b05d2e
Medusa Galadriel <medusaga@mi-w6.msk.21-school.ru>
2023-08-01 14:39:24 +0300

    squaring

--- src/squaring.c
+++ src/squaring.c
@@ -1,37 +1,43 @@
 #include <stdio.h>
+
 #define NMAX 10
 
-int input(int *a, int *n);
-void output(int *a, int n);
-void squaring(int *a, int n);
+int input(int*, int*);
+void output(int*, int);
+void squaring(int*, int);
 
-int main()
-{
+int main() {
+    //! showMemory(start=65520)
     int n, data[NMAX];
-    input(data, n);
+    input(data, &n);
     squaring(data, n);
     output(data, n);
 
     return 0;
 }
 
-int input(int *a, int *n)
-{
-    scanf("%d", n);
-    for(int *p = a; p - a < *n; p++)
-    {
-        scanf("%d", p);
+int input(int* ina, int* inn) {
+    if (scanf("%d", inn) != 1 && getchar() != ('\n' || ' ')) {
+        printf("n/a\n");
+        return 0;
+    }
+    for (int* p = ina; p - ina < *inn; p++) {
+        if (scanf("%d", p) != 1 && getchar() != ('\n' || ' ')) {
+            printf("n/a\n");
+            return 0;
+        }
     }
 }
 
-void output(int *a, int n)
-{
-    //NOTHING
+void output(int* outa, int outn) {
+    for (int i = 0; i < outn; i++) {
+        printf("%d ", outa[i]);
+    }
+    printf("\n");
 }
 
-void squaring(int *a, int n)
-{
-    //NOTHING
+void squaring(int* sqra, int sqrn) {
+    for (int i = 0; i < sqrn; i++) {
+        sqra[i] *= sqra[i];
+    }
 }
-
-
